22915ab2d7865122ca16e591bbaf125d2e5e0765
jamperezmondragon <70548653+jamperezmondragon@users.noreply.github.com>
2022-01-14 00:10:03 -0600

    Update A.cpp

--- COCI/2006-07/contest-1/A.cpp
+++ COCI/2006-07/contest-1/A.cpp
@@ -1,1 +1,14 @@
-test
+#include <bits/stdc++.h>
+using namespace std;
+//debuging
+bool debug = true;
+#define DEBUG if (debug)
+
+
+int main() {
+  set<int> s;
+  for (int i = 0; i < 10; i++) {
+    int x; cin >> x; s.insert(x%42);
+  }
+  cout << s.size();
+}
