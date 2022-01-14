c90ca5f4fc4d39e7e161f5b0eef98ede120cbbc8
jamperezmondragon <70548653+jamperezmondragon@users.noreply.github.com>
2022-01-14 00:20:02 -0600

    Create B.cpp

---
+++ COCI/2006-07/contest-1/B.cpp
@@ -0,0 +1,17 @@
+
+/*
+ * AC:
+ * https://evaluator.hsin.hr/tasks/HONI060712herman/
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+//debuging
+bool debug = true;
+#define DEBUG if (debug)
+
+
+int main() {
+  double pi = 3.141593, n; cin >> n;
+  cout << fixed << setprecision(6) << n*n*pi << endl << n*n*2;
+}
