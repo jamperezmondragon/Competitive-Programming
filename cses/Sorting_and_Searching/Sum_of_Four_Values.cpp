9f996cc88a459ced6bbbee2d9d0d2aab5ef69e7c
jamperezmondragon <70548653+jamperezmondragon@users.noreply.github.com>
2022-01-22 18:27:00 -0600

    Create Sum_of_Four_Values.cpp

---
+++ cses/Sorting_and_Searching/Sum_of_Four_Values.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+//debuging 
+const bool DEB = false;
+#define DEBUG if (DEB) 
+/* Informative
+* ==============
+* Status: 
+* link del problema: 
+* submission:
+*/
+/*
+* Comments
+* =========
+*
+*/
+/* Analisis
+* ==========
+*
+*/
+//int const M = 1000000007;
+ 
+int main() {
+  int n, x, cnt = 1; cin >> n >> x; vector<pair<int, int>> vec(n); 
+  for (auto &e : vec) {
+    cin >> e.first; e.second = cnt++;
+  }
+  sort(vec.begin(), vec.end());
+  for (int f = 0; f < n; f++) {
+    int y = x - vec[f].first;
+    for (int k = f + 1; k < n; k++) {
+      int target = y - vec[k].first;
+      for (int i = n - 1, j = 0; i > j;) {
+        if (i == k || i == f) {
+          i--; continue;
+        }
+        if (j == k || j == f) {
+          j++; continue;
+        }
+        if (vec[i].first + vec[j].first == target) {
+          cout << vec[f].second << " " << vec[k].second << " " << vec[j].second << " " << vec[i].second; return 0;
+        }
+        if (vec[i].first + vec[j].first < target) j++;
+        else i--;
+      }
+    }
+  }
+  cout << "IMPOSSIBLE";
+}
